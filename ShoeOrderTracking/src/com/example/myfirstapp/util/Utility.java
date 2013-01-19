f41c5b0775b213c5bd452f3bcea7f213c2995364
rav.shah@gmail.com <rav.shah@gmail.com@2ee161a7-1eb9-0261-f9ed-da9db35da067>
2013-01-19 11:56:01 +0000

    Re-factoring

--- ShoeOrderTracking/src/com/example/myfirstapp/util/Utility.java
+++ ShoeOrderTracking/src/com/example/myfirstapp/util/Utility.java
@@ -1,5 +1,15 @@
 package com.example.myfirstapp.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Color;
@@ -33,6 +43,35 @@ public class Utility {
 		alertDialogBuilder.setMessage("" + obj);
 		AlertDialog alertDialog = alertDialogBuilder.create();
 		alertDialog.show();
+	}
+
+	public static char[] getHttpRequest(String url) throws IOException,
+			ClientProtocolException {
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url);
+
+		request.setHeader("Accept", "application/json");
+		request.setHeader("Content-type", "application/json");
+
+		HttpResponse response = httpClient.execute(request);
+		HttpEntity responseEntity = response.getEntity();
+
+		// Read response data into buffer
+		char[] buffer = new char[(int) responseEntity.getContentLength()];
+		InputStream stream = responseEntity.getContent();
+		InputStreamReader reader = new InputStreamReader(stream);
+		reader.read(buffer);
+		stream.close();
+		return buffer;
+	}
+
+	public static String getNormalizedString(char[] response) {
+		String str = "";
+		for (char c : response) {
+			str += c;
+		}
+		str = str.replace("\"", "");
+		return str;
 	}
 
 }
