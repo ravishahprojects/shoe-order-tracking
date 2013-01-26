97f66be438a778b2973caaa039fc181d56fe6cc2
rav.shah@gmail.com <rav.shah@gmail.com@2ee161a7-1eb9-0261-f9ed-da9db35da067>
2013-01-26 20:22:40 +0000

    1. Introduced settings file. 2. Changes related to department name configuration if none is found. 3. Minor code changes

--- trunk/ShoeOrderTracking/src/com/example/myfirstapp/BackupClass.java
+++ trunk/ShoeOrderTracking/src/com/example/myfirstapp/BackupClass.java
@@ -1,5 +1,11 @@
 package com.example.myfirstapp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.widget.Button;
@@ -10,7 +16,9 @@ import android.widget.TextView;
  */
 public class BackupClass {
 
+	private static final String PROPERTIES_FILE = null;
 	private Context context;
+	private String deptName;
 
 	public Button createAndGetButton(final int id, final CharSequence text) {
 		final Button btn = new Button(context);
@@ -26,6 +34,31 @@ public class BackupClass {
 		tv.setText(text);
 		tv.setTextColor(Color.WHITE);
 		return tv;
+	}
+
+	public void writeData1() throws FileNotFoundException {
+		FileOutputStream fOut = null;
+		OutputStreamWriter myOutWriter = null;
+		try {
+			File file = new File(PROPERTIES_FILE);
+			file.createNewFile();
+			fOut = new FileOutputStream(file);
+			myOutWriter = new OutputStreamWriter(fOut);
+			deptName = "dept1";
+			myOutWriter.append(deptName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (myOutWriter != null)
+					myOutWriter.close();
+				if (fOut != null)
+					fOut.close();
+			} catch (IOException e) {
+				// Ignore
+			}
+
+		}
 	}
 
 }
--- trunk/ShoeOrderTracking/src/com/example/myfirstapp/MainActivity.java
+++ trunk/ShoeOrderTracking/src/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.myfirstapp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
+import java.util.Properties;
 
 import org.apache.http.client.ClientProtocolException;
 import org.json.JSONArray;
@@ -13,10 +18,12 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -26,22 +33,87 @@ import com.example.myfirstapp.listener.BtnClickListener;
 import com.example.myfirstapp.listener.SuccessHandler;
 import com.example.myfirstapp.util.Utility;
 
+// TODO: Currently the Settings View and Main Views are tightly coupled in this class. They should be separated.
 public class MainActivity extends Activity implements SuccessHandler {
 
 	final Context context = this;
+	private String deptName;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-				.permitAll().build();
-		StrictMode.setThreadPolicy(policy);
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		try {
+			handleSettingsFileReadWrite();
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+			super.onCreate(savedInstanceState);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		if (deptName != null)
+			setContentView(R.layout.activity_main);
 	}
 
-	public void handleButton(View v) {
+	private void handleSettingsFileReadWrite() throws FileNotFoundException {
 		try {
-			tryWCF();
+			File file = context.getFileStreamPath(Constants.PROPERTIES_FILE);
+			if (file.exists()) {
+				readData();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (deptName == null || deptName.isEmpty()) {
+			writeData();
+		}
+	}
+
+	private void writeData() {
+		try {
+			Properties properties = loadProperties(R.raw.settings);
+			String deptNameProp = properties.getProperty(Constants.DEPT_NAME);
+			if (deptNameProp == null || deptNameProp.isEmpty()) {
+				loadSettingsView();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void loadSettingsView() {
+		setContentView(R.layout.activity_settings);
+	}
+
+	private Properties loadProperties(int id) throws IOException {
+		InputStream rawResource = this.getResources().openRawResource(id);
+		Properties properties = new Properties();
+		properties.load(rawResource);
+		return properties;
+	}
+
+	private void readData() throws FileNotFoundException, IOException {
+		Properties properties = loadProperties(R.raw.settings);
+		deptName = properties.getProperty(Constants.DEPT_NAME);
+		Log.i("readData", "Property read from file :" + Constants.DEPT_NAME + ":" + deptName);
+	}
+
+	public void handleUpdateSettingsButton(View v) {
+		try {
+			EditText edtDeptName = (EditText) findViewById(R.id.edtDeptName);
+			deptName = edtDeptName.getText().toString();
+			Properties properties = loadProperties(R.raw.settings);
+			FileOutputStream outputStream = this.getResources().openRawResourceFd(R.raw.settings).createOutputStream();
+			properties.store(outputStream, "Storing Dept Name");
+			properties.setProperty(Constants.DEPT_NAME, deptName);
+			setContentView(R.layout.activity_main);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void handleRefreshButton(View v) {
+		try {
+			refreshOrderData();
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -51,38 +123,31 @@ public class MainActivity extends Activity implements SuccessHandler {
 		}
 	}
 
-	public void tryWCF() throws ClientProtocolException, IOException,
-			JSONException {
-		char[] response = requestData();
+	public void refreshOrderData() throws ClientProtocolException, IOException, JSONException {
+		char[] response = requestOrderData();
 		processGetWorkResponse(enhanceResponse(response));
 	}
 
-	private char[] requestData() throws IOException, ClientProtocolException {
-		String url = Constants.HTTP_SERVICE1_SVC + Constants.SEPARATOR
-				+ Constants.GET_WORK + Constants.SEPARATOR + "dept1";
-		char[] buffer = Utility.getHttpRequest(url);
-		return buffer;
+	private char[] requestOrderData() throws IOException, ClientProtocolException {
+		String url = Constants.HTTP_SERVICE1_SVC + Constants.SEPARATOR + Constants.GET_WORK + Constants.SEPARATOR + deptName;
+		return Utility.getHttpRequest(url);
 	}
 
 	private String enhanceResponse(char[] buffer) {
 		String responseString = new String(buffer);
-		responseString = responseString.substring(1,
-				responseString.length() - 1);
+		responseString = responseString.substring(1, responseString.length() - 1);
 		responseString = responseString.replace("\\", "");
 		return responseString;
 	}
 
-	private void processGetWorkResponse(String responseString)
-			throws JSONException {
+	private void processGetWorkResponse(String responseString) throws JSONException {
 		JSONArray vehicle = new JSONArray(responseString);
 		processViewForResponse(vehicle);
 	}
 
 	private void processViewForResponse(JSONArray result) throws JSONException {
 		LayoutInflater inflater = getLayoutInflater();
-
-		ScrollView sView = (ScrollView) inflater.inflate(
-				R.layout.activity_main, null);
+		ScrollView sView = (ScrollView) inflater.inflate(R.layout.activity_main, null);
 		TableLayout tableLayout = (TableLayout) sView.getChildAt(0);
 		for (int i = 0; i < result.length(); i++) {
 			int j = 0;
@@ -96,16 +161,14 @@ public class MainActivity extends Activity implements SuccessHandler {
 		setContentView(sView);
 	}
 
-	private void processKeys(int i, int j, TableRow tbrow, JSONObject row)
-			throws JSONException {
+	private void processKeys(int i, int j, TableRow tbrow, JSONObject row) throws JSONException {
 		Iterator keys = row.keys();
 		while (keys.hasNext()) {
 			j = processKey(i, j, tbrow, row, keys);
 		}
 	}
 
-	private int processKey(int i, int j, TableRow tbrow, JSONObject row,
-			Iterator keys) throws JSONException {
+	private int processKey(int i, int j, TableRow tbrow, JSONObject row, Iterator keys) throws JSONException {
 		CharSequence txtLabel;
 		Object next = keys.next();
 		String key = next.toString();
@@ -113,17 +176,14 @@ public class MainActivity extends Activity implements SuccessHandler {
 			int id = i * ++j;
 			txtLabel = row.getString(key);
 			if ("id".equalsIgnoreCase(key)) {
-				Button btn = Utility.createAndGetButton(
-						Integer.valueOf(txtLabel.toString()).intValue(),
-						Constants.UPDATE_WORK, context);
+				Button btn = Utility.createAndGetButton(Integer.valueOf(txtLabel.toString()).intValue(), Constants.UPDATE_WORK, context);
 				BtnClickListener clickListener = new BtnClickListener();
 				clickListener.setHandler(this);
 				btn.setOnClickListener(clickListener);
 				tbrow.addView(btn);
 			} else {
-				txtLabel = txtLabel + "|";
-				TextView view = Utility.createAndGetTextView(id, txtLabel,
-						context);
+				// txtLabel = txtLabel + "|";
+				TextView view = Utility.createAndGetTextView(id, txtLabel, context);
 				tbrow.addView(view);
 			}
 		}
