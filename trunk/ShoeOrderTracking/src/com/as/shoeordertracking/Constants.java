af43d0e178414e6b798b51edbee52a0bf3002736
ashah2311@gmail.com <ashah2311@gmail.com@2ee161a7-1eb9-0261-f9ed-da9db35da067>
2013-03-30 12:45:31 +0000

    I am in a hurry!!! just getting the code to work.

--- trunk/ShoeOrderTracking/src/com/as/shoeordertracking/Constants.java
+++ trunk/ShoeOrderTracking/src/com/as/shoeordertracking/Constants.java
@@ -7,10 +7,12 @@ public class Constants {
 
 	public static final String GET_WORK = "GetWork";
 	public static final String WS_UPDATE_WORK_API = "UpdateWork";
-	public static final String UPDATE_WORK = "Update Work";
-	public static final String UNDO = "Undo";
+	public static final String UPDATE_WORK_IN = "In";
+	public static final String UPDATE_WORK_OUT = "Out";
+	public static final String UNDO_IN = "Undo - In";
+	public static final String UNDO_OUT = "Undo - Out";
 	public static final String WS_UNDO_API = "UndoUpdateWork";
-	public static final String HTTP_SERVICE1_SVC = "http://192.168.100.1:8081/Service1.svc";
+	public static final String HTTP_SERVICE1_SVC = "http://192.168.1.19:8081/Service1.svc";
 	public static final String SEPARATOR = "/";
 	public static final String DEPT_NAME = "DEPT_NAME";
 	public static final String PROPERTIES_FILE = "settings.properties";
@@ -18,9 +20,11 @@ public class Constants {
 	public static final Set<String> keysToProcess = new HashSet<String>();
 
 	static {
-		keysToProcess.add("colour");
-		keysToProcess.add("id");
 		keysToProcess.add("ono");
+		keysToProcess.add("cname");
+		keysToProcess.add("style");
+		keysToProcess.add("colour");
+		keysToProcess.add("ssize");		
 	}
 
 	public static final String UPDATE_WORK_SUCCESS = "Work Updated";
--- trunk/ShoeOrderTracking/src/com/as/shoeordertracking/ShoeOrderTrackingActivity.java
+++ trunk/ShoeOrderTracking/src/com/as/shoeordertracking/ShoeOrderTrackingActivity.java
@@ -1,10 +1,14 @@
 package com.as.shoeordertracking;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Properties;
 
@@ -19,9 +23,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
@@ -37,7 +43,8 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 
 	final Context context = this;
 	private String deptName;
-
+	private String ipAddress;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		try {
@@ -55,16 +62,20 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 
 	private void handleSettingsFileReadWrite() throws FileNotFoundException {
 		try {
-			File file = context.getFileStreamPath(Constants.PROPERTIES_FILE);
+			//File file = context.getFileStreamPath(getFilesDir().toString() + "/settings.properties");
+			File file = new File(getFilesDir().toString() + "/settings.properties");
+			
 			if (file.exists()) {
 				readData();
+			} else {				
+				writeData();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if (deptName == null || deptName.isEmpty()) {
+		/* if (deptName == null || deptName.isEmpty()) {
 			writeData();
-		}
+		} */
 	}
 
 	private void writeData() {
@@ -72,6 +83,8 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 			Properties properties = loadProperties(R.raw.settings);
 			String deptNameProp = properties.getProperty(Constants.DEPT_NAME);
 			if (deptNameProp == null || deptNameProp.isEmpty()) {
+				loadSettingsView();
+			} else {
 				loadSettingsView();
 			}
 		} catch (IOException e) {
@@ -91,20 +104,43 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 	}
 
 	private void readData() throws FileNotFoundException, IOException {
-		Properties properties = loadProperties(R.raw.settings);
+		/* Properties properties = loadProperties(R.raw.settings);
 		deptName = properties.getProperty(Constants.DEPT_NAME);
-		Log.i("readData", "Property read from file :" + Constants.DEPT_NAME	+ ":" + deptName);
+		Log.i("readData", "Property read from file :" + Constants.DEPT_NAME	+ ":" + deptName); */
+		FileInputStream fis = openFileInput("settings.properties");
+	    InputStreamReader inputStreamReader = new InputStreamReader(fis);
+	    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+	    StringBuilder sb = new StringBuilder();
+	    String line;	    
+	    while ((line = bufferedReader.readLine()) != null) {
+	        sb.append(line);
+	        sb.append("\n");
+	    }
+		fis.close();
+		String[] lines = sb.toString().split("\\n");
+
+		deptName = lines[0];
+		ipAddress = lines[1];
 	}
 
 	public void handleUpdateSettingsButton(View v) {
 		try {
 			EditText edtDeptName = (EditText) findViewById(R.id.edtDeptName);
 			deptName = edtDeptName.getText().toString();
-			Properties properties = loadProperties(R.raw.settings);
-			FileOutputStream outputStream = this.getResources().openRawResourceFd(R.raw.settings).createOutputStream();
-			properties.store(outputStream, "Storing Dept Name");
-			properties.setProperty(Constants.DEPT_NAME, deptName);
-			setContentView(R.layout.activity_shoe_order_tracking);
+			EditText edtIPAddress = (EditText) findViewById(R.id.edtIPAddress);
+			ipAddress = edtIPAddress.getText().toString();
+			//Properties properties = loadProperties(R.raw.settings);
+			//FileOutputStream outputStream = this.getResources().openRawResourceFd(R.raw.settings).createOutputStream();
+			//properties.setProperty(Constants.DEPT_NAME, deptName);
+			//properties.store(outputStream, "Storing Dept Name");
+			//properties.store(new FileOutputStream("settings.properties"), null);
+			FileOutputStream fos = openFileOutput("settings.properties", Context.MODE_WORLD_READABLE);
+			fos.write(deptName.getBytes());
+			fos.write("\n".getBytes());
+			fos.write(ipAddress.getBytes());
+			fos.close();
+			//Log.i("something", getFilesDir().toString());
+			setContentView(R.layout.activity_shoe_order_tracking);			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -148,11 +184,13 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 		LayoutInflater inflater = getLayoutInflater();
 		ScrollView sView = (ScrollView) inflater.inflate(R.layout.activity_shoe_order_tracking, null);
 		TableLayout tableLayout = (TableLayout) sView.getChildAt(0);
+
 		for (int i = 0; i < result.length(); i++) {
 			int j = 0;
 			TableRow tbrow = new TableRow(this);
 			JSONObject row = result.getJSONObject(i);
 			processKeys(i, j, tbrow, row);
+			tbrow.setBackgroundResource(R.drawable.cell_shape);
 			tableLayout.addView(tbrow);
 		}
 		sView.removeAllViews();
@@ -163,9 +201,46 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 	@SuppressWarnings("rawtypes")
 	private void processKeys(int i, int j, TableRow tbrow, JSONObject row) throws JSONException {
 		Iterator keys = row.keys();
-		while (keys.hasNext()) {
+		/* while (keys.hasNext()) {
 			j = processKey(i, j, tbrow, row, keys);
+		} */
+		String s = "";
+		while (keys.hasNext()) {			
+			Object next = keys.next();
+			String key = next.toString();
+			if (Constants.keysToProcess.contains(key)) {
+				if (key.equalsIgnoreCase("ono")) {
+					s += "\nOrder Num: " + row.getString(key); 
+				} else if (key.equalsIgnoreCase("cname")) {
+					s += "\nCustomer: " + row.getString(key);
+				} else if (key.equalsIgnoreCase("style")) {
+					s += "\nStyle: " + row.getString(key);
+				} else if (key.equalsIgnoreCase("colour")) {
+					s += "\nColour: " + row.getString(key);
+				} else if (key.equalsIgnoreCase("ssize")) {
+					s += "\nSize: " + row.getString(key);
+				} 
+			}
 		}
+		Button view = Utility.createAndGetButton(10, s.substring(1), context, deptName);
+		tbrow.addView(view);
+		
+		
+		int id = Integer.valueOf(row.getString("id")).intValue();
+		
+		Button btn = Utility.createAndGetButton(id, Constants.UPDATE_WORK_IN, context, deptName);
+		BtnClickListener clickListener = new BtnClickListener();
+		clickListener.setHandler(this);
+		btn.setOnClickListener(clickListener);
+		tbrow.addView(btn);		
+		
+		Button btnOut = Utility.createAndGetButton(id, Constants.UPDATE_WORK_OUT, context, deptName);
+		BtnClickListener clickListenerOut = new BtnClickListener();
+		clickListenerOut.setHandler(this);
+		btnOut.setOnClickListener(clickListenerOut);
+		tbrow.addView(btnOut);
+		
+		tbrow.setGravity(Gravity.TOP);
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -177,14 +252,14 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 			int id = i * ++j;
 			txtLabel = row.getString(key);
 			if ("id".equalsIgnoreCase(key)) {
-				Button btn = Utility.createAndGetButton(Integer.valueOf(txtLabel.toString()).intValue(), Constants.UPDATE_WORK, context);
+				Button btn = Utility.createAndGetButton(Integer.valueOf(txtLabel.toString()).intValue(), Constants.UPDATE_WORK_IN, context, deptName);
 				BtnClickListener clickListener = new BtnClickListener();
 				clickListener.setHandler(this);
 				btn.setOnClickListener(clickListener);
 				tbrow.addView(btn);
 			} else {
 				// txtLabel = txtLabel + "|";
-				TextView view = Utility.createAndGetTextView(id, txtLabel, context, this);
+				TextView view = Utility.createAndGetTextView(txtLabel, context, this);
 				tbrow.addView(view);
 			}
 		}
@@ -201,12 +276,22 @@ public class ShoeOrderTrackingActivity extends Activity implements SuccessHandle
 	@Override
 	public void handleSuccess(View v) {
 		Button btn = (Button) v;
-		if (btn.getText().toString().equalsIgnoreCase(Constants.UPDATE_WORK)) {
+		if (btn.getText().toString().equalsIgnoreCase(Constants.UPDATE_WORK_IN)) {
 			btn.setBackgroundColor(Color.GREEN);
-			btn.setText(Constants.UNDO);
-		} else if (btn.getText().toString().equalsIgnoreCase(Constants.UNDO)) {
-			btn.setBackgroundColor(Color.GRAY);
-			btn.setText(Constants.UPDATE_WORK);
+			Date dt = new Date();
+			btn.setText(Constants.UNDO_IN + "\n" + dt.toString());
+		} else if (btn.getText().toString().toLowerCase().contains("undo")) {  //equalsIgnoreCase(Constants.UNDO_IN)) {
+			btn.setBackgroundColor(Color.DKGRAY);
+			//btn.setBackgroundResource(R.drawable.cell_shape);
+			btn.setText(Constants.UPDATE_WORK_IN);
+		} else if (btn.getText().toString().equalsIgnoreCase(Constants.UPDATE_WORK_OUT)) {
+			btn.setBackgroundColor(Color.GREEN);
+			Date dt = new Date();
+			btn.setText(Constants.UNDO_OUT + "\n" + dt.toString());			
+		}  else if (btn.getText().toString().toLowerCase().contains("undo")) {   // equalsIgnoreCase(Constants.UNDO_OUT)) {
+			btn.setBackgroundColor(Color.DKGRAY);
+			//btn.setBackgroundResource(R.drawable.cell_shape);
+			btn.setText(Constants.UPDATE_WORK_OUT);
 		}
 	}
 
